2c49af51dd7b5610a1c82eec7b694e0af0ab7717
VasilenkoVyacheslav <vasilenko.vyacheslav@gmail.com>
2021-09-25 14:05:02 +0300

    Lesson 11

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,15 @@ $(document).ready(function () {
     $('.mobile-menu').on('click', function () {
         $('.menu').slideToggle();
     })
+
+    const swiper = new Swiper('.gallery', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        slidesPerView: 4,
+        spaceBetween: 30,
+        speed: 400
+    });
 })
