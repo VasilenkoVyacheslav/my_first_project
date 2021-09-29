f5963fbf12206873601acfb02c9be32e4afbd868
VasilenkoVyacheslav <vasilenko.vyacheslav@gmail.com>
2021-09-29 21:32:18 +0300

    lesson 12

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,40 @@ $(document).ready(function () {
     })
 
     const swiper = new Swiper('.gallery', {
-        breakpoints: {
-            320: {
-                slidesPerView: 4,
-                spaceBetween: 30
-            }
-        },
         loop: true,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        slidesPerView: 4,
         spaceBetween: 30,
-        speed: 400
+        speed: 400,
+        breakpoints: {
+            320: {
+                slidesPerView: 1
+            },
+            560: {
+                slidesPerView: 2
+            },
+            768: {
+                slidesPerView: 3
+            },
+            900: {
+                slidesPerView: 4
+            }
+        }
+    });
+
+    $(function() {
+        var tab = $('#tabs .tabs-items > div');
+        tab.hide().filter(':first').show();
+
+        $('#tabs .tabs-nav a').click(function(){
+            tab.hide();
+            tab.filter(this.hash).show();
+            $('#tabs .tabs-nav a').removeClass('active');
+            $(this).addClass('active');
+            return false;
+        }).filter(':first').click();
     });
 
 })
