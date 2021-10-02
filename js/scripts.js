fe60ceccc3ed08bb2556bb972cfaa6d2b9ea6b87
VasilenkoVyacheslav <vasilenko.vyacheslav@gmail.com>
2021-10-02 12:04:11 +0300

    Lesson 13

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.mobile-menu').on('click', function () {
-        $('.menu').slideToggle();
+        $('.menu').slideToggle().css('right', '0px !important').css('left', 'auto');
     })
 
     const swiper = new Swiper('.gallery', {
@@ -9,8 +9,43 @@ $(document).ready(function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        slidesPerView: 4,
         spaceBetween: 30,
-        speed: 400
+        speed: 400,
+        breakpoints: {
+            320: {
+                slidesPerView: 1
+            },
+            560: {
+                slidesPerView: 2
+            },
+            768: {
+                slidesPerView: 3
+            },
+            900: {
+                slidesPerView: 4
+            }
+        }
     });
+    // Tabs script
+    $(function() {
+        var tab = $('#tabs .tabs-items > div');
+        tab.hide().filter(':first').show();
+
+        $('#tabs .tabs-nav a').click(function(){
+            tab.hide();
+            tab.filter(this.hash).show();
+            $('#tabs .tabs-nav a').removeClass('active');
+            $(this).addClass('active');
+            return false;
+        }).filter(':first').click();
+    });
+    // Anchor links
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+    
 })
